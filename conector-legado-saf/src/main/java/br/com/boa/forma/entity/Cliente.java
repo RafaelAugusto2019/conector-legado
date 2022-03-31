ff3b24acacc96d62a03380f620d4667dbb192f5e
Rafael Augusto <rafaelaugusto2019@gmail.com>
2022-03-30 22:25:36 -0300

    feat: consulta da tabela cliente

---
+++ conector-legado-saf/src/main/java/br/com/boa/forma/entity/Cliente.java
@@ -0,0 +1,54 @@
+package br.com.boa.forma.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Clientes")
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 3262725538575824946L;
+	@Id
+    @Column(name = "carteirinhaId")
+	private int carteirinhaId;
+	
+    @Column(name = "nome")
+	private String nome;
+    
+    @Column(name = "cpf")
+    private int cpf;
+    
+    @Column(name = "dataNascimento" )
+    private String dataNascimento;
+    
+	public int getCarteirinhaId() {
+		return carteirinhaId;
+	}
+	public void setCarteirinhaId(int carteirinhaId) {
+		this.carteirinhaId = carteirinhaId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+       
+}
